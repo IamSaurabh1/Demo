95411f8d9d3e206270e6aa9d83470a52a0358f3a
Saurabh Gupta <31432921+IamSaurabh1@users.noreply.github.com>
2020-03-18 20:40:21 +0530

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -4,10 +4,10 @@ public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
+        }
         else{
             System.out.println("I am not covered");
-        }
-            return;
+        return;
         }
         
     }

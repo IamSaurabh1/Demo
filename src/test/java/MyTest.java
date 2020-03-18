8bb84c24b8a70a33c6e7cb46a536fce8e1583809
Saurabh Gupta <31432921+IamSaurabh1@users.noreply.github.com>
2020-03-18 20:34:34 +0530

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -5,18 +5,18 @@ public class MyTest {
     @Test
     public void test_method_1() {
         Demo d = new Demo();
-        d.DoSomething(false);
+        d.DoSomething(true);
     }
 
-//     @Test
-//     public void test_method_2() {
-//         Demo d2 = new Demo();
-//         d2.DoSomething(false);
-//     }
-    
-//     @Test
-//     public void test_method_3() {
-//         Demo d3 = new Demo();
-//         d3.DoSomething(false);
-//     }
+     @Test
+     public void test_method_2() {
+         Demo d2 = new Demo();
+         d2.DoSomething(false);
+     }
+
+     @Test
+     public void test_method_3() {
+         Demo d3 = new Demo();
+         d3.DoSomething(false);
+     }
 }
